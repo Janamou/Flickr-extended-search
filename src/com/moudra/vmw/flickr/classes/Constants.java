bb41ac6d12ed39a9abd83b21cf1c5f15423c8f75
Jana Moudra <jana.moudra@gmail.com>
2011-11-20 18:51:32 +0100

    Uprava constants.

--- src/com/moudra/vmw/flickr/classes/Constants.java
+++ src/com/moudra/vmw/flickr/classes/Constants.java
@@ -1,6 +1,6 @@
 package com.moudra.vmw.flickr.classes;
 
 public class Constants {
-	public static final String API_KEY = "29dd6af04f48e1f79c8118d43dad79d4";
-	public static final String API_SECRET = "88f02557de8564ac";
+	public static final String API_KEY = "";
+	public static final String API_SECRET = "";
 }
