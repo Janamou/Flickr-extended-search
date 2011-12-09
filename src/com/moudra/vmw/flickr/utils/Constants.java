39f163edfce86def72cfdba9b338fa755674b98a
Jana Moudra <jana.moudra@gmail.com>
2011-12-09 14:04:44 +0100

    Konstanty.

---
+++ src/com/moudra/vmw/flickr/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.moudra.vmw.flickr.utils;
+
+public class Constants {
+	public static final String API_KEY = "";
+	public static final String API_SECRET = "";
+	public static final String MEDIA_TYPE = "photos";
+	public static final String GEO_UNITS = "km";
+}
